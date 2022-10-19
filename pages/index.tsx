2ea94d46c0cb704e9e38fecf7c6697f68f4c2e6c
Aaronkst <aaron.kst99@gmail.com>
2022-10-19 16:04:04 +0630

    deep linking, content update

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,20 @@ const Home: NextPage = () => {
         <p className="text-2xl font-black text-white text-center">My PWA</p>
       </section>
       <section className="lg:w-2/3 mx-auto bg-neutral-100 lg:bg-white p-5 min-h-screen">
-        {["More Exciting", "Features to", "take YOUR", "BREATH Away."].map(
-          (s) => (
-            <div
-              key={s}
-              className="p-2 my-4 drop-shadow-lg bg-white border-b-4 border-indigo-900 rounded-md"
-            >
-              <p className="text-4xl font-black">{s}</p>
-            </div>
-          )
-        )}
+        {[
+          "More Exciting",
+          "and Lavish",
+          "Features to",
+          "take YOUR",
+          "BREATH Away.",
+        ].map((s) => (
+          <div
+            key={s}
+            className="p-2 my-4 drop-shadow-lg bg-white border-b-4 border-indigo-900 rounded-md"
+          >
+            <p className="text-4xl font-black">{s}</p>
+          </div>
+        ))}
         <button className="p-2 my-4 bg-indigo-900 rounded-md text-white font-black text-center w-full hover:bg-indigo-600 hover:drop-shadow-lg">
           Keep me Posted &rarr;
         </button>
